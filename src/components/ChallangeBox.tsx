01cecdc50b02dc125e8b2a8cc8f9b36b28e280b4
rafAmorim8 <rafael_amorim30@hotmail.com>
2021-02-24 21:19:51 -0800

    Connects components to context

--- src/components/ChallangeBox.tsx
+++ src/components/ChallangeBox.tsx
@@ -3,23 +3,25 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengBox() {
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+
   return (
     <div className={styles.challengeBoxContainer}>
-      { hasActiveChallenge ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400xp</header>
+          <header>Ganhe {activeChallenge.amount}</header>
 
           <main>
-            <img src="icons/body.svg" alt="Body" />
+            <img src={`icons/${activeChallenge.type}.svg`} alt="Body" />
             <strong>Novo desafio</strong>
-            <p>Levante e faca uma caminhada de 3 minutos.</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
             <button
               type="button"
               className={styles.challengeFailedButton}
+              onClick={resetChallenge}
             >
               Falhei
             </button>
--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,8 +1,12 @@
-import gsap from 'gsap';
-import { useEffect } from 'react';
+import { useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
+import gsap from 'gsap';
 
 export function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
+
+  const percentToNextLevel = Math.round((currentExperience * 100 / experienceToNextLevel));
   useEffect(() => {
     gsap.from('.currentExperienceBar', { scaleX: 0, duration: 1, ease: 'circ' });
   }, []);
@@ -11,13 +15,13 @@ export function ExperienceBar() {
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div className="currentExperienceBar" style={{ width: '50%' }}>
-          <span className={styles.currentExperience} style={{ left: '50%' }}>
-            300 xp
+        <div className="currentExperienceBar" style={{ width: `${percentToNextLevel}%` }}>
+          <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
+            {currentExperience} xp
           </span>
         </div>
       </div>
-      <span>600 xp</span>
+      <span>{experienceToNextLevel} xp</span>
     </header>
   );
 }
