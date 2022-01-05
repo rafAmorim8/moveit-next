1421111ab2fd3cedb1860221290f3e6549a6cddf
rafAmorim8 <rafael_amorim30@hotmail.com>
2022-01-04 19:20:05 -0800

    Bug Fix: Experience bar width stays proportional to xp even when 0 xp

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -9,13 +9,13 @@ export function ExperienceBar() {
   const percentToNextLevel = Math.round((currentExperience * 100 / experienceToNextLevel));
   useEffect(() => {
     gsap.from('.currentExperienceBar', { scaleX: 0, duration: 1, ease: 'circ' });
-  }, []);
+  }, [currentExperience]);
 
   return (
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div className="currentExperienceBar" style={{ width: `${percentToNextLevel}%` }}>
+        <div className="currentExperienceBar" style={{ width: `${currentExperience > 0 ? percentToNextLevel : 0}%` }}>
           <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
             {currentExperience} xp
           </span>
